77d99e1de8779d451e389f9b827622360c67b16c
Sergio1327 <seregaan.serega2003@gmail.com>
2023-08-23 01:53:45 +0300

    feat(client :  страница продуктов): добавлен функционал добавления продукта в базу в модальном окне

--- projects_template/frontend/components/productModal.vue
+++ projects_template/frontend/components/productModal.vue
@@ -15,13 +15,25 @@
                 </div>
             </div>
             <div class="field">
+                <label class="label">Дата добавления продукта</label>
+                <div class="control">
+                    <input class="input" type="datetime-local" v-model="addedAt">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">теги продукта</label>
+                <div class="control">
+                    <input class="input" v-model="tags" type="text">
+                </div>
+            </div>
+            <div class="field">
                 <label class="label">Варианты продукта</label>
                 <div class="control">
                     <div v-for="(variant, index) in productVariants" :key="index">
                         <div class="field">
                             <label class="label">Масса</label>
                             <div class="control">
-                                <input class="input" v-model="variant.weight" type="text">
+                                <input class="input" v-model="variant.weight" type="number">
                             </div>
                         </div>
                         <div class="field">
@@ -31,7 +43,9 @@
                             </div>
                         </div>
                     </div>
-                    <button class="button is-primary" type="button" @click="addVariant">Добавить вариант</button>
+                    <div class="field is-flex is-justify-content-flex-end is-align-items-center">
+                        <button class="button is-primary mt-4" type="button" @click="addVariant">Добавить вариант</button>
+                    </div>
                 </div>
             </div>
             <div class="label">{{ resp }}</div>
@@ -48,6 +62,8 @@ export default {
         return {
             productName: "",
             descr: "",
+            addedAt: null,
+            tags: "",
             productVariants: [{
                 weight: "",
                 unit: ""
@@ -62,14 +78,47 @@ export default {
         addVariant() {
             this.productVariants.push({});
         },
-        submitModalData() {
-            // Здесь можно выполнить логику отправки данных на сервер,
-            // используя данные this.productName, this.descr и this.productVariants
-            console.log('Отправка данных на сервер:', {
-                productName: this.productName,
-                descr: this.descr,
-                productVariants: this.productVariants
-            });
+        async submitModalData() {
+            const formatedVariants = this.productVariants.map(v => ({
+                weight: parseInt(v.weight),
+                unit: v.unit
+            }))
+
+            const requestData = {
+                name: this.productName,
+                description: this.descr,
+                added_at: this.formatDate(this.addedAt),
+                tags: this.tags,
+                variants: formatedVariants
+            }
+            console.log(JSON.stringify(requestData))
+            try {
+                const response = await fetch("http://127.0.0.1:9000/product/add", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(requestData),
+                })
+
+                const responseData = await response.json()
+                console.log(responseData.Data)
+                this.resp = "продукт успешно добавлен в базу , ID продукта = " + responseData.Data.product_id
+            } catch (error) {
+                console.error("Ошибка при отправке запроса:", error);
+                console.log(error)
+            }
+        },
+        formatDate(dateTime) {
+            const date = new Date(dateTime);
+            const year = date.getUTCFullYear();
+            const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+            const day = String(date.getUTCDate()).padStart(2, "0");
+            const hours = String(date.getHours()).padStart(2, "0");
+            const minutes = String(date.getUTCMinutes()).padStart(2, "0");
+            const seconds = String(date.getUTCSeconds()).padStart(2, "0");
+
+            return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}+05:00`;
         }
     }
 }
