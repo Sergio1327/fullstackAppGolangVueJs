9aa21d2c0bddb6915e37c4885219c63bec94bdfb
Sergio1327 <seregaan.serega2003@gmail.com>
2023-08-22 07:46:13 +0300

    feat(client:страница продуктов): добавлены поля для добавления вариантов  продукта в модальное окно

--- projects_template/frontend/components/productModal.vue
+++ projects_template/frontend/components/productModal.vue
@@ -5,35 +5,53 @@
             <div class="field">
                 <label class="label">Название продукта</label>
                 <div class="control">
-                    <input class="input" v-model="productName" type="tex">
+                    <input class="input" v-model="productName" type="text">
                 </div>
             </div>
             <div class="field">
-                <label class="label">ID склада</label>
+                <label class="label">Описание продукта</label>
                 <div class="control">
-                    <input class="input" type="number">
+                    <input class="input" v-model="descr" type="text">
                 </div>
             </div>
             <div class="field">
-                <label class="label">Кол-во продуктов</label>
+                <label class="label">Варианты продукта</label>
                 <div class="control">
-                    <input class="input" type="number">
+                    <div v-for="(variant, index) in productVariants" :key="index">
+                        <div class="field">
+                            <label class="label">Масса</label>
+                            <div class="control">
+                                <input class="input" v-model="variant.weight" type="text">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <label class="label">Единица измерения</label>
+                            <div class="control">
+                                <input class="input" v-model="variant.unit" type="text">
+                            </div>
+                        </div>
+                    </div>
+                    <button class="button is-primary" type="button" @click="addVariant">Добавить вариант</button>
                 </div>
             </div>
-            <div class="label"> {{ resp }}</div>
-            <button class="button is-primary" @click="submitModalData">добавить продажу</button>
+            <div class="label">{{ resp }}</div>
+            <button class="button is-primary" @click="submitModalData">Добавить продажу</button>
             <button class="button" @click="closeModal">Закрыть</button>
         </div>
         <button class="modal-close is-large" aria-label="Закрыть" @click="closeModal"></button>
-
     </div>
 </template>
-
+  
 <script>
 export default {
     data() {
         return {
             productName: "",
+            descr: "",
+            productVariants: [{
+                weight: "",
+                unit: ""
+            }],
             resp: ""
         };
     },
@@ -41,6 +59,19 @@ export default {
         closeModal() {
             this.$emit('closeModal');
         },
+        addVariant() {
+            this.productVariants.push({});
+        },
+        submitModalData() {
+            // Здесь можно выполнить логику отправки данных на сервер,
+            // используя данные this.productName, this.descr и this.productVariants
+            console.log('Отправка данных на сервер:', {
+                productName: this.productName,
+                descr: this.descr,
+                productVariants: this.productVariants
+            });
+        }
     }
 }
 </script>
+  
